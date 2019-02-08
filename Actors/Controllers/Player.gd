1ecb5d96d2edd4a78b9255dbe130dea5739258c6
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2019-02-08 09:13:16 -0400

    Player controller waits for input

--- Actors/Controllers/Player.gd
+++ Actors/Controllers/Player.gd
@@ -2,6 +2,25 @@ extends ActorController
 
 class_name Player
 
+signal _input_processed
+
+func _ready() -> void:
+	._ready()
+	set_process_unhandled_input(false)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		_process_key_input(event)
+
+func _process_key_input(event: InputEventKey) -> void:
+	if event.pressed:
+		emit_signal("_input_processed")
+
 func get_action() -> void:
-	yield(get_tree(), "idle_frame")
+	print("press key to continue")
+
+	set_process_unhandled_input(true)
+	yield(self, "_input_processed")
+	set_process_unhandled_input(false)
+
 	print(get_actor().name, ": Player")

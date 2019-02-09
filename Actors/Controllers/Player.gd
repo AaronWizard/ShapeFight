8c9c8678561b3ecd8be566833d62c6c6c8ebabb7
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2019-02-08 21:23:12 -0400

    Player keyboard movement

--- Actors/Controllers/Player.gd
+++ Actors/Controllers/Player.gd
@@ -2,29 +2,34 @@ extends ActorController
 
 class_name Player
 
-signal _input_processed
+signal _input_processed(action)
 
 func _ready() -> void:
 	._ready()
 	set_process_unhandled_input(false)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		_process_key_input(event as InputEventKey)
+	if Input.is_action_just_pressed("move_north"):
+		_try_move(Direction.NORTH)
+	elif Input.is_action_just_pressed("move_east"):
+		_try_move(Direction.EAST)
+	elif Input.is_action_just_pressed("move_south"):
+		_try_move(Direction.SOUTH)
+	elif Input.is_action_just_pressed("move_west"):
+		_try_move(Direction.WEST)
 
-func _process_key_input(event: InputEventKey) -> void:
-	if event.pressed:
-		emit_signal("_input_processed")
+func _try_move(direction: int):
+	var new_cell : Vector2 = \
+			get_actor().cell_position + Direction.VECTORS[direction]
+	if get_map().actor_can_enter_cell(get_actor(), new_cell):
+		var action = MoveAction.new()
+		action.actor = get_actor()
+		action.direction = direction
+		emit_signal("_input_processed", action)
 
 func get_action() -> ActorAction:
-	print("press key to continue")
-
 	set_process_unhandled_input(true)
-	yield(self, "_input_processed")
+	var action : ActorAction = yield(self, "_input_processed")
 	set_process_unhandled_input(false)
 
-	print(get_actor().name, ": Player")
-
-	var action := ActorAction.new()
-	action.actor = get_actor()
 	return action

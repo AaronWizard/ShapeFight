0e719daf2020deede33732f6221c9380f3d8d5b5
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2019-08-08 08:03:11 -0300

    Limit and condense _input_processed signals in Player

--- Actors/Controllers/Player.gd
+++ Actors/Controllers/Player.gd
@@ -15,16 +15,22 @@ func _ready() -> void:
 	_walk_path = []
 
 func _unhandled_input(_event: InputEvent) -> void:
+	var action: ActorAction = null
+	var is_waiting := false
+
 	if Input.is_action_pressed('move_north'):
-		_try_move(Direction.NORTH)
+		action = _try_move(Direction.NORTH)
 	elif Input.is_action_pressed('move_east'):
-		_try_move(Direction.EAST)
+		action = _try_move(Direction.EAST)
 	elif Input.is_action_pressed('move_south'):
-		_try_move(Direction.SOUTH)
+		action = _try_move(Direction.SOUTH)
 	elif Input.is_action_pressed('move_west'):
-		_try_move(Direction.WEST)
+		action = _try_move(Direction.WEST)
 	elif Input.is_action_just_pressed('wait'):
-		emit_signal('_input_processed', null)
+		is_waiting = true
+
+	if action or is_waiting:
+		emit_signal('_input_processed', action)
 
 func receive_walk_path(path: Array) -> void:
 	assert( is_processing_unhandled_input() )
@@ -32,31 +38,43 @@ func receive_walk_path(path: Array) -> void:
 	_walk_path += path # Make sure items in path are copied
 	assert(_walk_path.front() == get_actor().cell_position)
 	_walk_path.pop_front()
-	_try_walk_path()
 
-func _try_move(direction: int) -> void:
+	var action := _try_walk_path()
+	if action:
+		emit_signal('_input_processed', action)
+
+func _try_move(direction: int) -> ActorAction:
+	var action: ActorAction = null
+
 	var new_cell: Vector2 = \
 			get_actor().cell_position + Direction.VECTORS[direction]
 	if get_map().actor_can_enter_cell(get_actor(), new_cell, false):
-		_create_move_action(direction)
+		action = _create_move_action(direction)
 	else:
-		_try_attack(new_cell)
+		action = _try_attack(new_cell)
 
-func _create_move_action(direction: int) -> void:
+	return action
+
+func _create_move_action(direction: int) -> ActorAction:
 	var action := MoveActionScene.instance() as MoveAction
 	action.actor = get_actor()
 	action.direction = direction
-	emit_signal('_input_processed', action)
+	return action
 
-func _try_attack(cell: Vector2) -> void:
+func _try_attack(cell: Vector2) -> ActorAction:
+	var action: ActorAction = null
+
 	var other_actor := get_map().get_actor_at_cell(cell)
 	if (other_actor != null) and (other_actor != get_actor()):
-		var action := AttackActionScene.instance() as AttackAction
+		action = AttackActionScene.instance() as AttackAction
 		action.actor = get_actor()
 		action.target = other_actor
-		emit_signal('_input_processed', action)
 
-func _try_walk_path() -> void:
+	return action
+
+func _try_walk_path() -> ActorAction:
+	var action: ActorAction = null
+
 	if _walk_path.size() > 0:
 		var cell = _walk_path.front()
 		_walk_path.pop_front()
@@ -64,13 +82,18 @@ func _try_walk_path() -> void:
 		if get_map().actor_can_enter_cell(get_actor(), cell, false):
 			var direction := Direction.get_closest_direction_diff(
 					get_actor().cell_position, cell)
-			_create_move_action(direction)
+			action = _create_move_action(direction)
+
+	return action
 
 func get_action() -> void:
+	var action: ActorAction = null
+
 	set_process_unhandled_input(true)
 
-	call_deferred('_try_walk_path')
-	var action : ActorAction = yield(self, '_input_processed')
+	action = _try_walk_path()
+	if not action:
+		action = yield(self, '_input_processed')
 
 	set_process_unhandled_input(false)
 

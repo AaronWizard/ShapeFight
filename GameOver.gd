25c0cfd88609c99001601bdf441f40dc7c4062d5
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2019-08-15 22:45:26 -0300

    Game over screen UI

---
+++ GameOver.gd
@@ -0,0 +1,10 @@
+extends VBoxContainer
+
+func _on_Reload_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_MainMenu_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()

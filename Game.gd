8693b2ff95bb71d20f6ade5332f6bfd477290c39
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2019-08-15 21:24:59 -0300

    Game over screen

--- Game.gd
+++ Game.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# warning-ignore:unused_class_variable
+export(PackedScene) var game_over_scene
+
 onready var game_over_transition := $Transitions/GameOverTransition
 
 onready var stamina := $GUI/Stamina as Range
@@ -22,17 +25,6 @@ func _set_actor_events() -> void:
 		#warning-ignore:return_value_discarded
 		actor.connect('died', self, '_actor_died', [actor])
 
-func _on_Hero_took_damage(damage: int) -> void:
-	stamina.value -= damage
-	if hero.stats.stamina <= 0:
-		game_over_transition.show()
-		game_over_transition.play()
-
-func _actor_died(actor: Actor) -> void:
-	map.remove_child(actor)
-	if actor == hero:
-		turn_processor.running = false
-
 func _on_TurnProcessor_turn_started(actor) -> void:
 	if actor == hero:
 		overlay.actor = hero
@@ -43,3 +35,20 @@ func _on_TurnProcessor_turn_ended(actor) -> void:
 	if actor == hero:
 		overlay.actor = null
 		overlay.disconnect('clicked_for_path', hero.controller, 'receive_walk_path')
+
+func _on_Hero_took_damage(damage: int) -> void:
+	stamina.value -= damage
+
+func _actor_died(actor: Actor) -> void:
+	map.remove_child(actor)
+	if actor == hero:
+		_game_over()
+
+func _game_over() -> void:
+	turn_processor.running = false
+
+	game_over_transition.show()
+	game_over_transition.play()
+	yield(game_over_transition, 'animation_finished')
+
+	assert(get_tree().change_scene_to(game_over_scene) == OK)
